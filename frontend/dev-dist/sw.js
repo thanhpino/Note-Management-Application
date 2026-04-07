43d94b958b4a0c26193f7b1ed27ef119341b847f
thanhpino <tt3145539@gmail.com>
2026-04-07 10:18:29 +0700

    feat: add label for role clearly and fixing toast notification

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.5s8rqc92p5g"
+    "revision": "0.v261ffklp64"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
