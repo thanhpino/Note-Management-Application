ecaec99cc3616407977bea42e65b2640e00e0cf2
thanhpino <tt3145539@gmail.com>
2026-05-14 13:51:24 +0700

    fix: configure frontend_url in app config to support activation links on port 5173

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.sp5rd7n3vog"
+    "revision": "0.1tesmuts4qk"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
